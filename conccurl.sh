efd58863ea66fb6dd2ec9caed83a0df49725e6ca
Isak Eriksson <isaker@student.chalmers.se>
2016-12-15 11:33:34 +0100

    Prepares for optional task

--- conccurl.sh
+++ conccurl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in `seq 1 10`; do
-curl -d 'entry=t'${i} -X 'POST /board' 'http://129.16.24.216:63118' &
-curl -d 'entry=t'${i} -X 'POST /board' 'http://129.16.24.216:63119' &
-curl -d 'entry=t'${i} -X 'POST /board' 'http://129.16.24.216:63120' &
+curl -d 'entry=a'${i} -X 'POST /board' 'http://202.141.161.43:63159' &
+curl -d 'entry=b'${i} -X 'POST /board' 'http://195.113.161.83:63159' &
+curl -d 'entry=c'${i} -X 'POST /board' 'http://128.112.139.19:63159' &
 done
--- debug.sh
+++ debug.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -d 'id=63120' -X 'DEBUG' 'http://129.16.24.211:63119/'
+curl -d 'id=63120' -X 'DEBUG' 'http://129.16.24.206:63120/'
+echo "Changed network connecivity for 63120"
